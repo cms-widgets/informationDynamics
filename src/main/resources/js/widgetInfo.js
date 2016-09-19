84ac58468ed9ce4b2f4d0cc508cc97982f72e08a
lihuaixin <1072931117@qq.com>
2016-09-19 14:50:56 +0800

    js优化验证

--- src/main/resources/js/widgetInfo.js
+++ src/main/resources/js/widgetInfo.js
@@ -9,6 +9,11 @@ CMSWidgets.initWidget({
             this.properties.serial = $(".category").val();
             this.properties.title = $('select.category option:selected').text() + '';
             this.properties.size = $(".size").val();
+            if (this.properties.serial == null || this.properties.serial == '' || this.properties.size == ''
+                || this.properties.size == '0') {
+                onFailed("数据源serial和展示条数不能为空和0");
+                return;
+            }
             onSuccess(this.properties);
             return this.properties;
         },
